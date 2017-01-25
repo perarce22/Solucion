49d2116fe64ae5bdd8b624d5850823eb00ab12f0
perarce22 <perarce22@gmail.com>
2017-01-25 17:40:03 -0500

    Add files via upload

---
+++ Banco.Util/Config.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco.Util
+{
+    public static class Config
+    {
+        public static string CadenaConexion
+        {
+            get
+            {
+                return System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            }
+        }
+    }
+}
---
+++ Banco.Util/DataReaderExtensions.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Data;
+
+namespace Banco.Util
+{
+    public static class DataReaderExtensions
+    {
+        public static Int16 GetValueInt16(this IDataReader reader, int ordinal)
+        {
+            Int16 value = 0;
+
+            if (!reader.IsDBNull(ordinal))
+            {
+                value = reader.GetInt16(ordinal);
+            }
+
+            return value;
+        }
+
+        public static int GetValueInt32(this IDataReader reader, int ordinal)
+        {
+            int value = 0;
+
+            if (!reader.IsDBNull(ordinal))
+            {
+                value = reader.GetInt32(ordinal);
+            }
+
+            return value;
+        }
+
+        public static int? GetValueNullInt32(this IDataReader reader, int ordinal)
+        {
+            int? value = null;
+
+            if (!reader.IsDBNull(ordinal))
+            {
+                value = reader.GetInt32(ordinal);
+            }
+
+            return value;
+        }
+
+        public static decimal GetValueDecimal(this IDataReader reader, int ordinal)
+        {
+            decimal value = 0;
+
+            if (!reader.IsDBNull(ordinal))
+            {
+                value = reader.GetDecimal(ordinal);
+            }
+
+            return value;
+        }
+        public static decimal? GetValueNullDecimal(this IDataReader reader, int ordinal)
+        {
+            decimal? value = null;
+
+            if (!reader.IsDBNull(ordinal))
+            {
+                value = reader.GetDecimal(ordinal);
+            }
+
+            return value;
+        }
+
+        public static string GetValueString(this IDataReader reader, int ordinal)
+        {
+            string value = string.Empty;
+
+            if (!reader.IsDBNull(ordinal))
+            {
+                value = reader.GetString(ordinal);
+            }
+
+            return value;
+        }
+
+        public static DateTime GetValueDateTime(this IDataReader reader, int ordinal)
+        {
+            DateTime value = DateTime.MinValue;
+
+            if (!reader.IsDBNull(ordinal))
+            {
+                value = reader.GetDateTime(ordinal);
+            }
+
+            return value;
+        }
+        public static DateTime? GetValueNullDateTime(this IDataReader reader, int ordinal)
+        {
+            DateTime? value = null;
+
+            if (!reader.IsDBNull(ordinal))
+            {
+                value = reader.GetDateTime(ordinal);
+            }
+
+            return value;
+        }
+
+        public static string GetValueNullDateTimeString(this IDataReader reader, int ordinal)
+        {
+            var value = "__/__/____";
+
+            if (!reader.IsDBNull(ordinal))
+            {
+                value = reader.GetDateTime(ordinal).ToShortDateString().PadLeft(10,'0');
+            }
+            return value;
+        }
+
+        public static Boolean GetValueBoolean(this IDataReader reader, int ordinal)
+        {
+            bool value = false;
+
+            if (!reader.IsDBNull(ordinal))
+            {
+                value = reader.GetBoolean(ordinal);
+            }
+
+            return value;
+        }
+
+        public static Guid GetValueGuid(this IDataReader reader, int ordinal)
+        {
+            Guid value = default(Guid);
+
+            if (!reader.IsDBNull(ordinal))
+            {
+                value = reader.GetGuid(ordinal);
+            }
+
+            return value;
+        }
+
+        public static byte GetValueByte(this IDataReader reader, int ordinal)
+        {
+            byte value = 0;
+
+            if (!reader.IsDBNull(ordinal))
+            {
+                value = reader.GetByte(ordinal);
+            }
+
+            return value;
+        }
+    }
+}
